81734ef1f3bae37a7f69c7ad1b0faacd20698e12
Frederick Ofori <fofori031@st.ug.edu.gh>
2024-08-14 11:54:41 +0000

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderPage from './components/HeaderPage';
 
 function App() {
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product) => {
+    console.log('Product added to cart:', product);
+    const existingProduct = cart.find((item) => item._id === product._id);
+    if (existingProduct) {
+      setCart(
+        cart.map((item) =>
+          item._id === product._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const updateCartQuantity = (id, quantity) => {
+    setCart(
+      cart.map((item) => (item._id === id ? { ...item, quantity } : item))
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter((item) => item._id !== id));
+  };
+
+  const cartCount = cart.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderPage cartCount={cartCount} setSearchResults={setSearchResults} />
     </div>
   );
 }
