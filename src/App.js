19b383524382ed776a3fef9aefabf27491a1a129
Frederick Ofori <fofori031@st.ug.edu.gh>
2024-08-17 13:29:15 +0000

    Added Vercel Analytics component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HeaderPage from "./components/HeaderPage";
@@ -78,6 +79,7 @@ function App() {
         <Route path="/support" element={<CustomerSupport/>} />
       </Routes>
       <ScrollToTopButton />
+      <Analytics />
     </BrowserRouter>
   );
 }
